80de2bb138cd570d352b68d3478fb01a089c3fa4
Arturo Hinostroza <83926355+Kinartb@users.noreply.github.com>
2023-04-26 06:11:09 -0500

    Desarrollo el archivo en conjunto con Miguel Vega

---
+++ Practica1-C3S2/Sprint 2/SOSgame/src/Sprint1/Board.java
@@ -0,0 +1,18 @@
+package Sprint1;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("SOS game");
+        frame.setContentPane(new GuiSOS().Pantalla);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(600,600);
+        frame.setLocation(500,100);
+        frame.setVisible(true);
+    }
+}
