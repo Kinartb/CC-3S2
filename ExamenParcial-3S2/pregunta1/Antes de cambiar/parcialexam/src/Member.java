7316246642177954ecadbd7256fa7f23ffb3ead4
Arturo Hinostroza <83926355+Kinartb@users.noreply.github.com>
2023-05-16 00:01:20 -0500

    Delete ExamenParcial-3S2 directory

--- ExamenParcial-3S2/pregunta1/Antes de cambiar/parcialexam/src/Member.java
+++
@@ -1,8 +0,0 @@
-public abstract class Member {
-    private final String nombre;
-    public Member(String nombre) {
-        this.nombre = nombre;
-    }
-    public abstract void joinTournament();
-    public abstract void organizeTournament();
-}
